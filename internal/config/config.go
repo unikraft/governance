100a1730010b5ddd93f56638fc5e8478ebfe3033
Alexander Jung <alex@unikraft.io>
2023-09-28 08:30:11 +0000

    fix(config): Add GitHub user to the config
    This will be used when pushing to remote authenticated repositories, to
    be used instead of an SSH key.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,6 +10,7 @@ type Config struct {
 	DryRun         bool   `long:"dry-run" short:"D" env:"GOVERN_DRY_RUN" usage:"Do not perform any actual change."`
 	TeamsDir       string `long:"teams-dir" short:"T" env:"GOVERN_TEAMS_DIR" usage:"Path to the teams definition directory" default:"teams"`
 	ReposDir       string `long:"repos-dir" short:"r" env:"GOVERN_REPOS_DIR" usage:"Path to the repos definition directory" default:"repos"`
+	GithubUser     string `long:"github-user" env:"GOVERN_GITHUB_USER" usage:"GitHub User account name"`
 	GithubToken    string `long:"github-token" env:"GOVERN_GITHUB_TOKEN" usage:"GitHub API token"`
 	GithubEndpoint string `long:"github-endpoint" env:"GOVERN_GITHUB_ENDPOINT" short:"E" usage:"Alternative GitHub API endpoint (usually GitHub enterprise)"`
 	GithubSkipSSL  bool   `long:"github-skip-ssl" short:"S" env:"GOVERN_GITHUB_SKIP_SSL" usage:"Skip SSL check with GitHub API endpoint"`
