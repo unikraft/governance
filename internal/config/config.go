c8885c8cec895d5236ffa77627c55feca08607f2
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2023-12-12 14:18:16 +0200

    feat(internal): Add option to skip fancy rendering
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,6 +12,7 @@ type Config struct {
 	GithubEndpoint string `long:"github-endpoint" env:"GOVERN_GITHUB_ENDPOINT" short:"E" usage:"Alternative GitHub API endpoint (usually GitHub enterprise)"`
 	GithubSkipSSL  bool   `long:"github-skip-ssl" short:"S" env:"GOVERN_GITHUB_SKIP_SSL" usage:"Skip SSL check with GitHub API endpoint"`
 	LogLevel       string `long:"log-level" short:"l" env:"GOVERN_LOG_LEVEL" usage:"Log level verbosity" default:"info"`
+	NoRender       bool   `long:"no-render" env:"GOVERN_NO_RENDER" usage:"Do not render the output"`
 	ReposDir       string `long:"repos-dir" short:"r" env:"GOVERN_REPOS_DIR" usage:"Path to the repos definition directory" default:"repos"`
 	TeamsDir       string `long:"teams-dir" short:"T" env:"GOVERN_TEAMS_DIR" usage:"Path to the teams definition directory" default:"teams"`
 	TempDir        string `long:"temp-dir" short:"j" env:"GOVERN_TEMP_DIR" usage:"Temporary directory to store intermediate git clones"`
